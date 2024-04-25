45ce2251d59fc08acfb0043e2fec6026fdf36de6
Thalene <latriasouls@protonmail.com>
2024-04-25 01:46:46 -0300

    list new

--- src/0.3/main.hs
+++ src/0.3/main.hs
@@ -1,3 +1,9 @@
 main = do
     let lostNumbers = [[0,1,2,3],[2,3,4,5],[2,4,2,3]]
     print lostNumbers
+
+    let abc = ['a'..'z']
+    print abc
+
+    let parNumbers = [2,4..13]
+    print parNumbers
